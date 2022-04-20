0b935ea6a4a4293c1a2cf3a14830ce7eb0128a33
XingZhou <329201962@qq.com>
2022-04-20 16:26:16 +0800

    整理数据增广代码以及数据集导入代码

--- data_augmentation.py
+++ data_augmentation.py
@@ -5,15 +5,17 @@
 # @Email：329201962@qq.com
 from math import ceil
 
-import librosa
-import random
 import soundfile
 import re
 import colorednoise as cn
-import numpy as np
 import data_input_helper
 from matplotlib import pyplot as plt
 from itertools import chain
+import random
+import cv2
+import librosa
+import numpy as np
+import pandas as pd
 
 """
 原始数据的格式
@@ -52,6 +54,100 @@ Stream size                              : 85.3 KiB (100%)
 口径：
 40，50，80，100，150.....1800mm
 """
+# change wave data to stft
+from matplotlib import pyplot as plt
+
+
+def to_sp(x, n_fft=512, hop_length=256):
+    stft = librosa.stft(x, n_fft=n_fft, hop_length=hop_length)
+    sp = librosa.amplitude_to_db(np.abs(stft))
+    return sp
+
+
+# add white noise
+def add_whitenoise(x, rate=0.002):
+    return to_sp(x + rate * np.random.randn(len(x)))
+
+
+# add pink noise
+# https://www.dsprelated.com/showarticle/908.php
+def add_pinknoise(x, ncols=11, alpha=0.002):
+    """Generates pink noise using the Voss-McCartney algorithm.
+
+    nrows: number of values to generate
+    rcols: number of random sources to add
+
+    returns: NumPy array
+    """
+    nrows = len(x)
+    array = np.empty((nrows, ncols))
+    array.fill(np.nan)
+    array[0, :] = np.random.random(ncols)
+    array[:, 0] = np.random.random(nrows)
+
+    # the total number of changes is nrows
+    n = nrows
+    cols = np.random.geometric(0.5, n)
+    cols[cols >= ncols] = 0
+    rows = np.random.randint(nrows, size=n)
+    array[rows, cols] = np.random.random(n)
+
+    df = pd.DataFrame(array)
+    df.fillna(method='ffill', axis=0, inplace=True)
+    total = df.sum(axis=1)
+
+    return to_sp(alpha * total.values + x)
+
+
+# add random line
+def draw_line(x, length=[5, 20], thickness_length=[2, 4]):
+    result = np.copy(x)
+    width = x.shape[1]
+    height = x.shape[0]
+    angle = [0, np.pi / 2, np.pi, np.pi * 3 / 2]
+    np.random.shuffle(angle)
+
+    length = np.random.randint(length[0], length[1])
+    x1 = random.randint(length, width - length)
+    x2 = x1 + length * np.cos(angle[0])
+    y1 = random.randint(length, height - length)
+    y2 = y1 + length * np.sin(angle[0])
+
+    thickness = random.randint(thickness_length[0], thickness_length[1])
+    color1 = float(np.max(x))
+
+    cv2.line(result, (x1, y1), (int(np.min([width, x2])), int(np.min([height, y2]))), color1, thickness)
+
+    return result
+
+
+# change Hz to average
+def average_Hz(x, length=[2, 4]):
+    result = np.copy(x)
+    height = x.shape[0]
+
+    length = np.random.randint(length[0], length[1])
+    begin = np.random.randint(0, height - length)
+    for i in range(length):
+        result[begin + i] = np.mean(result[begin + i])
+
+    return result
+
+
+def normalization(x):  # 有待改进
+    max_ = 15.6  # np.max(x_train)
+    min_ = -74.7  # np.min(x_train)
+    mean = 133.4
+    sigma = 31.595366
+
+    result = cv2.resize(x, (224, 224))
+    result = (result - min_) / (max_ - min_)
+    return (result * 255 - mean) / sigma
+
+
+def to_img2(x):
+    result = cv2.resize(to_sp(x), (224, 224))
+    return np.array(result)
 
 
 # 对比不同数据的波形
@@ -217,6 +313,53 @@ def get_normal(root_target):  # 进行粗粒度的shuffle得到的新的正常�
         file = root_dir + '/1111-' + tag_list[1] + '-' + tag_list[2] + '-1' + tag_list[3][1:] + '.wav'
         soundfile.write(file, y_shuffle, 8000)
     return
+
+
+def demo_transfer():
+    audio1 = './data/noise/1/1487-867201051535215-20220325101238-13070100.wav'
+    y1, sr1 = librosa.load(audio1, sr=8000)
+    target = np.copy(y1)
+    print(to_sp(target).shape)
+
+    img0 = to_img2(target)
+    img0 = normalization(img0)
+
+    img1 = draw_line(img0)
+
+    img2 = add_whitenoise(target)
+    img2 = normalization(img2)
+
+    img3 = add_pinknoise(target)
+    img3 = normalization(img3)
+
+    img4 = average_Hz(img0)
+
+    plt.figure(figsize=(20, 20))
+    plt.subplot(1, 5, 1)
+    plt.axis("off")
+    plt.title("normal data")
+    plt.imshow(img0)
+
+    plt.subplot(1, 5, 2)
+    plt.axis("off")
+    plt.title("slight line")
+    plt.imshow(img1)
+
+    plt.subplot(1, 5, 3)
+    plt.axis("off")
+    plt.title("add white noise")
+    plt.imshow(img2)
+
+    plt.subplot(1, 5, 4)
+    plt.axis("off")
+    plt.title("add pink noise")
+    plt.imshow(img3)
+
+    plt.subplot(1, 5, 5)
+    plt.axis("off")
+    plt.title("average Hz")
+    plt.imshow(img4)
+    plt.show()
 
 
 if __name__ == "__main__":
--- dataset_loaders.py
+++ dataset_loaders.py
@@ -2,7 +2,8 @@
 # @Time : 2021/12/8 16:51
 # @Author : XingZhou
 # @Email : 329201962@qq.com
-
+import cv2
+import sklearn
 from torch.utils.data.dataset import Dataset
 import os
 import data_input_helper
@@ -121,7 +122,7 @@ class DaHenDataGfcc(Dataset):
         return len(self.data_path)
 
 
-class WaterPipeData(Dataset):
+class WaterPipeData(Dataset):  # 直接将原始数据的短时傅里叶变化后直接作为网络输入
     def __init__(self, root_dir, sub_dir):
         self.root_dir = root_dir
         self.sub_dir = sub_dir
@@ -134,10 +135,12 @@ class WaterPipeData(Dataset):
         x, sr = data_input_helper.load_clip(data_item_path)
         # 时变频谱图
         # 调用librosa的stft方法可以直接得到短时傅里叶变换的结果
-        stftX = librosa.stft(x)
+        stftX = librosa.stft(x, n_fft=512, hop_length=256)
         # 把幅度转成分贝格式
         Xdb = librosa.amplitude_to_db(abs(stftX))
-        return Xdb, data_name[-12]
+        Xdb_re = cv2.resize(Xdb, (224, 224))
+        Xdb_norma = sklearn.preprocessing.scale(Xdb_re, axis=1)
+        return Xdb_norma, data_name[-12]
 
     def __len__(self):
         return len(self.data_path)
